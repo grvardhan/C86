d3cbbd6634fe8378db179299fb1c35652d91ac61
grvardhan <76835880+grvardhan@users.noreply.github.com>
2021-08-25 20:46:37 +0530

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,149 @@
+import React, { Component } from "react";
+import { AsyncStorage, StyleSheet, Text, View } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import firebase from "firebase"
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      isEnabled:false,
+      light_theme:true,
+      profile_image:"",
+      name:""
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+    this.fetchUser()
+  }
+
+  async fetchUser(){
+    let theme,name,image;
+    await  firebase
+  .database()
+  .ref("/Users/"+firebase.auth().currentUser.uid)
+
+  .on("value",function(snapshot){
+    theme=snapshot.val().current_theme
+    name='${snapshot.val().first_name} ${snapshot.val().last_name}'
+    image=snapshot.val().profile_picture
+  })
+  this.setState({
+    light_theme:theme==="light"?ture:false,
+    isEnabled:theme==="light"?ture:false,
+name:name,
+profile_image:image    
+  })
+  }
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <Image
+              source={require("../assets/logo.png")}
+              style={styles.appIcon}
+            ></Image>
+            <Text style={styles.appTitleText}>{`Storytelling\nApp`}</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.signInWithGoogleAsync()}
+            >
+              <Image
+                source={require("../assets/google_icon.png")}
+                style={styles.googleIcon}
+              ></Image>
+              <Text style={styles.googleText}>Sign in with Google</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.cloudContainer}>
+            <Image
+              source={require("../assets/cloud.png")}
+              style={styles.cloudImage}
+            ></Image>
+          </View>
+        </View>
+      );
+    }
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.4,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  appIcon: {
+    width: RFValue(130),
+    height: RFValue(130),
+    resizeMode: "contain"
+  },
+  appTitleText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans"
+  },
+  buttonContainer: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    width: RFValue(250),
+    height: RFValue(50),
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    borderRadius: RFValue(30),
+    backgroundColor: "white"
+  },
+  googleIcon: {
+    width: RFValue(30),
+    height: RFValue(30),
+    resizeMode: "contain"
+  },
+  googleText: {
+    color: "black",
+    fontSize: RFValue(20),
+    fontFamily: "Bubblegum-Sans"
+  },
+  cloudContainer: {
+    flex: 0.3
+  },
+  cloudImage: {
+    position: "absolute",
+    width: "100%",
+    resizeMode: "contain",
+    bottom: RFValue(-5)
+  }
+});
+
